a8856f5e3ec3d4345848842227a20d0c857ac227
Samu Reinikainen <samu.reinikainen@gmail.com>
2022-12-13 15:36:18 +0200

    foo

--- ui_main_form.py
+++ ui_main_form.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\main_form.ui'
+# Form implementation generated from reading ui file '/home/samu/PyPicMover/main_form.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.7
 #
